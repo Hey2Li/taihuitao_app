a014526843d5dbde5f61af3784d667985d4a9170
Lee2Go <hey2li@163.com>
2017-08-17 10:44:10 +0800

    初始化

---
+++ taihuitao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taihuitao
+//
+//  Created by Tebuy on 2017/8/17.
+//  Copyright © 2017年 Tebuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ taihuitao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  taihuitao
+//
+//  Created by Tebuy on 2017/8/17.
+//  Copyright © 2017年 Tebuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
