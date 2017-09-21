1dbfa08239bc098b7e53de2f31839d053d8c5db4
Lee2Go <hey2li@163.com>
2017-09-21 10:56:49 +0800

    完成个人页面UI

---
+++ taihuitao/PersonalTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalTableViewController.h
+//  taihuitao
+//
+//  Created by Tebuy on 2017/9/21.
+//  Copyright © 2017年 Tebuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalTableViewController : UITableViewController
+
+@end
